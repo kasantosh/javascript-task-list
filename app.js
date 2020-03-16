e034c1c1e324120ff9c6134d4ff5a73c9ae32482
Santosh Kumar <kasantosh@yahoo.com>
2020-03-16 17:50:48 -0400

    Able to delete from local storage

--- app.js
+++ app.js
@@ -114,7 +114,19 @@ function removeTask(e) {
 }
 
 function removeTaskFromLocalStorage(taskItem) {
-  console.log(taskItem);
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'))
+  }
+
+  tasks.forEach(function (task, index) {
+    if (taskItem.textContent === task) {
+      tasks.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // Clear all tasks
